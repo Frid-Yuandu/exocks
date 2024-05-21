409d25dc88fce9cb02b15a573e6947ec6a0f0662
frid_yuandu <fird_wong@outlook.com>
2024-05-21 10:37:32 +0800

    Rewrote the test cases to be closer to real-world use cases.

--- apps/server/test/server_test.exs
+++ apps/server/test/server_test.exs
@@ -1,115 +1,124 @@
 defmodule ServerTest do
   use ExUnit.Case
-  import ExUnit.CaptureLog
   doctest Server
 
-  @timeout 6 * 1000
+  # @timeout 6 * 1000
   @server_port Application.compile_env(:server, :local_port)
 
-  setup do
-    pid = Server.start()
-    on_exit(:stop_listener, fn -> Server.stop(pid) end)
+  @no_auth 0x00
+  @userpass 0x02
+  @user_pass_version 0x01
+
+  @socks_version 0x05
+  @tcp 0x01
+  @ipv4 0x01
+  # @domain 0x03
+  # @ipv6 0x04
+  @req_succeeded 0x00
+
+  setup_all do
+    {:ok, _} = Server.start_link([])
+    on_exit(:stop_listener, fn -> Server.stop() end)
+
+    {:ok, ls} = :gen_tcp.listen(14579, [:binary, active: false, reuseaddr: true])
+    on_exit(:stop_dest_server, fn -> :gen_tcp.close(ls) end)
+
+    %{listen_sock: ls}
   end
 
-  test "total proxy procedure" do
-    # spawn a process simulating target server
-    simulate_dst_server(fn server ->
-      assert {:ok, "ping"} = :gen_tcp.recv(server, 0)
-      :ok = :gen_tcp.send(server, "pong")
-      assert {:ok, "hello"} = :gen_tcp.recv(server, 0)
-      :ok = :gen_tcp.send(server, "world")
-    end)
+  test "should proxy with no auth, ipv4, tcp connect", %{listen_sock: ls} do
+    address_type = @ipv4
+    command = @tcp
+    uri = <<127, 0, 0, 1, 14579::16>>
 
-    {:ok, s} =
-      :gen_tcp.connect(
-        {127, 0, 0, 1},
-        @server_port,
-        [:binary, active: false, reuseaddr: true]
-      )
+    neg_sent = <<@socks_version, 1, 0>>
+    neg_wanted = <<@socks_version, 0>>
+    req_sent = <<@socks_version, command, 0, address_type, uri::binary>>
+    req_wanted = <<@socks_version, @req_succeeded, 0, @ipv4, 127, 0, 0, 1, @server_port::16>>
+    msg_sent = "ping"
+    msg_wanted = "pong"
 
-    :ok = :gen_tcp.send(s, <<5, 1, 0>>)
-    {:ok, <<5, 0>>} = :gen_tcp.recv(s, 0)
-
-    :ok =
-      :gen_tcp.send(
-        s,
-        <<5, 1, 0x00, 1, 127, 0, 0, 1, 443::16>>
-      )
-
-    {:ok, <<5, 0x00, 0x00, 0x01, 127, 0, 0, 1, @server_port::16>>} =
-      :gen_tcp.recv(s, 0)
-
-    Process.sleep(500)
-    :ok = :gen_tcp.send(s, "ping")
-    assert {:ok, "pong"} = :gen_tcp.recv(s, 0)
-
-    Process.sleep(500)
-    :ok = :gen_tcp.send(s, "hello")
-    assert {:ok, "world"} = :gen_tcp.recv(s, 0)
-  end
-
-  # test "negotiate invalid version" do
-  #   {:ok, s} =
-  #     :gen_tcp.connect(
-  #       {127, 0, 0, 1},
-  #       @server_port,
-  #       [:binary, active: false, reuseaddr: true]
-  #     )
-
-  #   assert capture_log(fn ->
-  #            :ok = :gen_tcp.send(s, <<4, 1, 0>>)
-  #            assert {:ok, <<0x05, 0xFF>>} = :gen_tcp.recv(s, 0)
-  #          end) =~ "invalid negotiate version"
-  # end
-
-  test "negotiate unacceptable method" do
-    {:ok, s} =
-      :gen_tcp.connect(
-        {127, 0, 0, 1},
-        @server_port,
-        [:binary, active: false, reuseaddr: true]
-      )
-
-    :ok = :gen_tcp.send(s, <<5, 1, 0xFE>>)
-    assert {:ok, <<0x05, 0xFF>>} = :gen_tcp.recv(s, 0)
-  end
-
-  # test "negotiate receive timeout" do
-  #   {:ok, _} =
-  #     :gen_tcp.connect(
-  #       {127, 0, 0, 1},
-  #       @server_port,
-  #       [:binary, active: false, reuseaddr: true]
-  #     )
-
-  #   assert capture_log(fn ->
-  #            Process.sleep(@timeout)
-  #          end) =~ "receive timeout"
-  # end
-
-  # test "stop listener" do
-  #   assert capture_log(fn ->
-  #            pid = spawn(Server.Listener, :run, [])
-  #            send(pid, :listen)
-
-  #            :gen_tcp.connect(:localhost, @server_port, [
-  #              :binary,
-  #              active: false,
-  #              reuseaddr: true
-  #            ])
-
-  #            send(pid, :stop)
-  #            Process.sleep(1000)
-  #          end) =~ "server stop listenning"
-  # end
-
-  @spec simulate_dst_server((port() -> any())) :: any()
-  def simulate_dst_server(behaviour) do
     Task.start_link(fn ->
-      {:ok, sock} = :gen_tcp.listen(443, [:binary, active: false, reuseaddr: true])
-      {:ok, server} = :gen_tcp.accept(sock)
+      {:ok, sock} = :gen_tcp.accept(ls)
 
-      behaviour.(server)
+      {:ok, ^msg_sent} = :gen_tcp.recv(sock, 0)
+      :ok = :gen_tcp.send(sock, "pong")
+
+      :gen_tcp.close(sock)
     end)
+
+    connect_to_exocks()
+    |> send_recv(send: neg_sent, wanted: {:ok, neg_wanted})
+    |> send_recv(send: req_sent, wanted: {:ok, req_wanted})
+    |> send_recv(send: msg_sent, wanted: {:ok, msg_wanted})
+    |> :gen_tcp.close()
+  end
+
+  test "should proxy with userpass, ipv4, tcp connect", %{listen_sock: ls} do
+    address_type = @ipv4
+    command = @tcp
+    uri = <<127, 0, 0, 1, 14579::16>>
+
+    neg_sent = <<@socks_version, 1, @userpass>>
+    neg_wanted = <<@socks_version, @userpass>>
+    userpass_sent = <<@user_pass_version, 4, "user", 4, "pass">>
+    userpass_wanted = <<@user_pass_version, 0>>
+    req_sent = <<@socks_version, command, 0, address_type, uri::binary>>
+    req_wanted = <<@socks_version, @req_succeeded, 0, @ipv4, 127, 0, 0, 1, @server_port::16>>
+    msg_sent = "ping"
+    msg_wanted = "pong"
+
+    Task.start_link(fn ->
+      {:ok, sock} = :gen_tcp.accept(ls)
+
+      {:ok, ^msg_sent} = :gen_tcp.recv(sock, 0)
+      :ok = :gen_tcp.send(sock, "pong")
+
+      :gen_tcp.close(sock)
+    end)
+
+    connect_to_exocks()
+    |> send_recv(send: neg_sent, wanted: {:ok, neg_wanted})
+    |> send_recv(send: userpass_sent, wanted: {:ok, userpass_wanted})
+    |> send_recv(send: req_sent, wanted: {:ok, req_wanted})
+    |> send_recv(send: msg_sent, wanted: {:ok, msg_wanted})
+    |> :gen_tcp.close()
+  end
+
+  test "should not proxy with wrong userpass, ipv4, tcp connect", %{listen_sock: ls} do
+    address_type = @ipv4
+    command = @tcp
+    uri = <<127, 0, 0, 1, 14579::16>>
+
+    neg_sent = <<@socks_version, 1, @userpass>>
+    neg_wanted = <<@socks_version, @userpass>>
+    userpass_sent = <<@user_pass_version, 8, "username", 8, "password">>
+    userpass_wanted = <<@user_pass_version, 1>>
+
+    connect_to_exocks()
+    |> send_recv(send: neg_sent, wanted: {:ok, neg_wanted})
+    |> send_recv(send: userpass_sent, wanted: {:ok, userpass_wanted})
+    |> :gen_tcp.close()
+  end
+
+  test "should proxy with no acceptable", %{listen_sock: ls} do
+    neg_sent = <<@socks_version, 2, 1, 3>>
+    neg_wanted = <<@socks_version, 0xFF>>
+
+    connect_to_exocks()
+    |> send_recv(send: neg_sent, wanted: {:ok, neg_wanted})
+    |> :gen_tcp.close()
+  end
+
+  defp connect_to_exocks do
+    opts = [:binary, active: false, reuseaddr: true]
+    {:ok, sock} = :gen_tcp.connect(:localhost, @server_port, opts)
+    sock
+  end
+
+  defp send_recv(sock, send: sent, wanted: wanted) do
+    :ok = :gen_tcp.send(sock, sent)
+    assert wanted == :gen_tcp.recv(sock, 0)
+    sock
   end
 end
